5a2e4ba034020c9e4afc0ea4564c23629732d13c
Ricardo <smells78@hotmail.com>
2022-11-01 22:00:50 -0300

    Mais Uma

--- components/cabecalho.jsx
+++ components/cabecalho.jsx
@@ -1,8 +1,12 @@
+import Image from "next/image" 
+
+
 export default function Cabecalho() {
     return (
         <header>
             <h1>Fundamentos de Next.js e React</h1>
             <h2>Cabeçalho usando componente</h2>
+            <img src="/natha-e-seu-pai.png" alt="imagem" />
         </header>
     )
 }
